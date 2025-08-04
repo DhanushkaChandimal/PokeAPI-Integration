d5cd6bc1ceddbdc70cf7881bd56d494f13c596f4
Dhanushka <dhanushkamcr@gmail.com>
2025-08-04 14:47:03 -0500

    Handle errors

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 const txtName = document.getElementById("pokemonName");
 const container = document.getElementById("container");
 
-async function ferchPokemon(name){
+async function fetchPokemon(name){
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    if (!response.ok) throw new Error("Can't fetch data. Invalid name or ID");
     const pokemon = await response.json();
     // console.log(txtName.value);
     // console.log(response);
@@ -13,22 +14,32 @@ async function ferchPokemon(name){
 
 async function getPokemon(){
     container.innerHTML = "";
-    const pokemon = await ferchPokemon(txtName.value);
-    // console.log(pokemon);
-    const title = document.createElement("h3");
-    title.innerText = pokemon.name;
-    container.appendChild(title);
-    
-    const image = document.createElement("img");
-    image.src = `https://img.pokemondb.net/artwork/${pokemon.name}.jpg`;
-    image.alt = "Pokemon Image"
-    container.appendChild(image);
-    
-    // console.log(pokemon.types[0].type.name);
-    // console.log("Type: " + pokemon.types[0].type.name + (pokemon.types.length > 1 ? " and " + pokemon.types[1].type.name : ""))
-    const type = document.createElement("p");
-    type.innerText = "Type: "
-    + pokemon.types[0].type.name
-    + (pokemon.types.length > 1 ? " and " + pokemon.types[1].type.name : "");
-    container.appendChild(type);
+
+    try{
+        if(txtName.value.trim() === "") throw new Error("Please enter ID or Name");
+        const pokemon = await fetchPokemon(txtName.value);
+        // console.log(pokemon);
+        const title = document.createElement("h3");
+        title.innerText = pokemon.name;
+        container.appendChild(title);
+        
+        const image = document.createElement("img");
+        image.src = `https://img.pokemondb.net/artwork/${pokemon.name}.jpg`;
+        image.alt = "Pokemon Image"
+        container.appendChild(image);
+        
+        // console.log(pokemon.types[0].type.name);
+        // console.log("Type: " + pokemon.types[0].type.name + (pokemon.types.length > 1 ? " and " + pokemon.types[1].type.name : ""))
+        const type = document.createElement("p");
+        type.innerText = "Type: "
+        + pokemon.types[0].type.name
+        + (pokemon.types.length > 1 ? " and " + pokemon.types[1].type.name : "");
+        container.appendChild(type);
+    }catch (e){
+        const error = document.createElement("p");
+        error.innerText = e;
+        error.classList.add("errorMessage");
+        // console.log(e);
+        container.appendChild(error);
+    }
 }
