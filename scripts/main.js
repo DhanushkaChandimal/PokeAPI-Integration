42eb1a76d00d0e917b1c296120c694823f751ec7
Dhanushka <dhanushkamcr@gmail.com>
2025-08-04 12:26:55 -0500

    Fetch data and display pokemon dynamically

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,34 @@
 const txtName = document.getElementById("pokemonName");
+const container = document.getElementById("container");
 
-async function getPokemon(){
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${txtName.value}`);
+async function ferchPokemon(name){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const pokemon = await response.json();
     // console.log(txtName.value);
     // console.log(response);
     // console.log(pokemon);
-    console.log(pokemon.name);
+    // console.log(pokemon.name);
+    return await pokemon;
+}
+
+async function getPokemon(){
+    container.innerHTML = "";
+    const pokemon = await ferchPokemon(txtName.value);
+    // console.log(pokemon);
+    const title = document.createElement("h3");
+    title.innerText = pokemon.name;
+    container.appendChild(title);
+    
+    const image = document.createElement("img");
+    image.src = `https://img.pokemondb.net/artwork/${pokemon.name}.jpg`;
+    image.alt = "Pokemon Image"
+    container.appendChild(image);
+    
+    // console.log(pokemon.types[0].type.name);
+    // console.log("Type: " + pokemon.types[0].type.name + (pokemon.types.length > 1 ? " and " + pokemon.types[1].type.name : ""))
+    const type = document.createElement("p");
+    type.innerText = "Type: "
+    + pokemon.types[0].type.name
+    + (pokemon.types.length > 1 ? " and " + pokemon.types[1].type.name : "");
+    container.appendChild(type);
 }
